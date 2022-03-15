16d065a971584b4f6bd8bf76f6c4aca2ebc99d97
Sahan Samarakoon <sysamarakoon@gmail.com>
2022-03-15 11:46:43 +0530

    Add pitchfork turtle graphics

---
+++ pitchfork.py
@@ -0,0 +1,28 @@
+import turtle
+import turtle
+p = turtle.Pen()
+q = turtle.Pen()
+r = turtle.Pen()
+s = turtle.Pen()
+
+p.forward(200)
+p.right(90)
+q.forward(200)
+q.left(90)
+p.forward(50)
+q.forward(50)
+p.left(90)
+q.right(90)
+p.forward(50)
+q.forward(50)
+r.forward(150)
+r.right(90)
+s.forward(150)
+s.left(90)
+s.forward(100)
+r.forward(100)
+s.right(90)
+s.forward(100)
+r.left(90)
+r.forward(100)
+
